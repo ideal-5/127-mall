78b32d2be7cee3c62865c0e6ad0a6ce23f37667b
ideal <ide_al@163.com>
2025-03-24 19:08:36 +0800

    feat: 添加地址增加选择省市区的弹窗

--- src/pages/my-tool/address-add.vue
+++ src/pages/my-tool/address-add.vue
@@ -1,19 +1,89 @@
 <script setup lang="ts">
 import { ref, computed, onMounted } from "vue";
+import pcaData from "@/constants/pca-code.json";
 
-const text = ref("");
+interface Params {
+    region: string;
+    address: string;
+    name: string;
+    phone: string;
+}
+
+interface Address {
+    name: string;
+    id: string;
+}
+interface Pca {
+    code: string;
+    name: string;
+    children?: Pca[];
+}
+
+const pca: Pca[] = pcaData;
+
+const params = ref<Params>({
+    region: "",
+    address: "",
+    name: "",
+    phone: "",
+});
+
+const showCityPopup = ref(false);
+const province = computed<Address[]>(() => {
+    return pca.map((item) => ({
+        name: item.name,
+        id: item.code,
+    }));
+});
+const city = ref<Address[]>([{ id: "00", name: "未知" }]);
+const country = ref<Address[]>([{ id: "6", name: "未知" }]);
+
+const activeAddress = ref<{ province: Address; city: Address; country: Address }>({
+    province: { name: "", id: "" },
+    city: { name: "", id: "" },
+    country: { name: "", id: "" },
+});
+
+const cityChange = (e: { next?: string; value?: Address; custom: string }) => {
+    const { next, value } = e;
+    if (!value?.name) return;
+
+    const activeProvince = pca.find(
+        (item) => item.code === (next === "city" ? value.id : activeAddress.value.province.id)
+    );
+    if (!activeProvince) return;
+
+    if (next === "city") {
+        activeAddress.value.province = value;
+        city.value = activeProvince.children?.map(({ name, code }) => ({ name, id: code })) || [];
+    } else if (next === "country") {
+        activeAddress.value.city = value;
+        const activeCity = activeProvince.children?.find(({ code }) => code === value.id);
+        country.value = activeCity?.children?.map(({ name, code }) => ({ name, id: code })) || [];
+    }
+};
+
+type closeEvent = { province: Address; city: Address; country: Address };
+const addressClose = (e: { data: closeEvent; type: string }) => {
+    let {
+        data: { province, city, country },
+        type,
+    } = e;
+    activeAddress.value = { province, city, country };
+    params.value.region = `${activeAddress.value.province.name} ${activeAddress.value.city.name} ${activeAddress.value.country.name}`;
+};
 </script>
 
 <template>
     <div class="main flex flex-col items-center">
         <NavBar style="--my-navbar-border-bottom-color: #f2f2f2">添加地址</NavBar>
-        <div class="wfull flex">
+        <div class="wfull flex" @click="showCityPopup = true">
             <div class="w100 flex-shrink-0 flex-center">
                 <div class="i-mdi:map-marker-radius"></div>
             </div>
             <div class="flex-1 min-w0 border-b-solid border-b-1rpx border-b-#D6D6D6">
                 <nut-input
-                    v-model="text"
+                    v-model="params.region"
                     placeholder="请输入所在区域"
                     :border="false"
                     style="--nut-input-padding: 10px 0; --nut-input-font-size: 24rpx"
@@ -30,7 +100,7 @@ const text = ref("");
             </div>
             <div class="flex-1 min-w0 border-b-solid border-b-1rpx border-b-#D6D6D6">
                 <nut-input
-                    v-model="text"
+                    v-model="params.address"
                     placeholder="请输入详细地址与门牌号"
                     :border="false"
                     style="--nut-input-padding: 10px 0; --nut-input-font-size: 24rpx"
@@ -47,7 +117,7 @@ const text = ref("");
             </div>
             <div class="flex-1 min-w0 border-b-solid border-b-1rpx border-b-#D6D6D6">
                 <nut-input
-                    v-model="text"
+                    v-model="params.name"
                     placeholder="请输入收货人姓名"
                     :border="false"
                     style="--nut-input-padding: 10px 0; --nut-input-font-size: 24rpx"
@@ -64,7 +134,7 @@ const text = ref("");
             </div>
             <div class="flex-1 min-w0 border-b-solid border-b-1rpx border-b-#D6D6D6">
                 <nut-input
-                    v-model="text"
+                    v-model="params.phone"
                     placeholder="请输入手机号码"
                     :border="false"
                     style="--nut-input-padding: 10px 0; --nut-input-font-size: 24rpx"
@@ -78,5 +148,14 @@ const text = ref("");
 
         <div class="w660 h72 flex-center text-#fff text-30 font-500 bg-#FFAA48 b-rd-full mt-100">保存地址</div>
     </div>
-</template>
 
+    <nut-address
+        v-model:visible="showCityPopup"
+        :province="province"
+        :city="city"
+        :country="country"
+        @change="cityChange"
+        @close="addressClose"
+        custom-address-title="请选择所在地区"
+    ></nut-address>
+</template>
